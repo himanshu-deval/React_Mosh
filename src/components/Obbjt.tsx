b2d9d894387b29a4f189673025413de9db287dd6
Himanshu Deval <118205130+himanshu-deval@users.noreply.github.com>
2024-06-21 11:22:47 +0000

    Updating Nested Objects

--- src/components/Obbjt.tsx
+++ src/components/Obbjt.tsx
@@ -1,26 +1,27 @@
-import React, { useState } from 'react';
-import Button from "./button/Button"
+import React, { useState } from "react";
+import Button from "./button/Button";
 
-const Obbjt=()=>{
-  const [obj,setObj]=useState({
-    Drink:'Americano',
-    price:50,
-  })
+const Obbjt = () => {
+  const [obj, setObj] = useState({
+    Name: "Rahul",
+    address: {
+      city: "Delhi",
+      zipcode: 110019,
+    },
+  });
 
-  const handleClick=()=>{
-    const newObj={
-      Drink:'Coffee',
-      price:40
-    }
-    // const x=obj.Drink='Tea';
-    setObj(newObj);
-   
-  }
-  return(
+  const handleClick = () => {
+    setObj({ ...obj, address: { ...obj, city: "Jind", zipcode: 126113 } });
+  };
+  return (
     <>
-    <h1>Got this {obj.Drink} For {obj.price}</h1>
-    <button onClick={handleClick} text="Click">Child</button>
+      <h1>
+        I am {obj.Name} and i live in {obj.address.city} {obj.address.zipcode}
+      </h1>
+      <button onClick={handleClick} text="Click">
+        Child
+      </button>
     </>
-  )
-}
+  );
+};
 export default Obbjt;
