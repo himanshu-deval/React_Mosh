2893fc27a4071eaf2cd59a47bcd63db8540e8025
Himanshu Deval <118205130+himanshu-deval@users.noreply.github.com>
2024-07-02 05:14:06 +0000

    Making states Dynamic using UseState Hook

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,30 +1,52 @@
-import React, { ReactElement, useRef } from "react";
+import React, { ReactElement, useRef, useState } from "react";
 
 const Form = () => {
-  const nameRef=useRef<HTMLInputElement>(null);
-  const ageRef=useRef<HTMLInputElement>(null);
-  const person={name:'', age:0};
-const handleSubmit=(event: ReactElement)=>{
-  event.preventDefault();
-  if(nameRef.current!==null && ageRef.current !==null){
- person.name= nameRef.current.value;    
- person.age=parseInt(ageRef.current.value);
+  const [person, setPerson] = useState({
+    Name: "",
+    age: 0,
+  });
+  const handleSubmit = (event: ReactElement) => {
+    event.preventDefault();
+    console.log("Submitted");
     console.log(person);
-  }
-}
-  
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="mb-3">
-        <label htmlFor="name" className="form-label">Name</label>
-        <input ref={nameRef} id="name" className="form-control" type="text"></input>
-      </div>
-      <div className="mb-3">
-        <label htmlFor="age" className="form-label">Age</label>
-        <input ref={ageRef} id="age" className="form-control" type="Number"></input>
-      </div>
-      <button className="btn btn-primary" type="submit">Submit</button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input
+            onChange={(event) =>
+              setPerson({ ...person, Name: event.target.value })
+            }
+            id="name"
+            value={person.Name}
+            className="form-control"
+            type="text"
+          ></input>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="age" className="form-label">
+            Age
+          </label>
+          <input
+            onChange={(event) =>
+              setPerson({ ...person, age: parseInt(event.target.value) })
+            }
+            id="age"
+            value={person.age}
+            className="form-control"
+            type="Number"
+          ></input>
+        </div>
+        <button className="btn btn-primary" type="submit">
+          Submit
+        </button>
+      </form>
+    </>
   );
 };
 export default Form;
