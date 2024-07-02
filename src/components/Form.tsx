25ab418b13fb767efa0515a68150b4a2d82288cd
Himanshu Deval <118205130+himanshu-deval@users.noreply.github.com>
2024-07-02 04:28:17 +0000

    using UseRef for accessing the input fields

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { ReactElement, useRef } from "react";
 
 const Form = () => {
+  const nameRef=useRef<HTMLInputElement>(null);
+  const ageRef=useRef<HTMLInputElement>(null);
+  const person={name:'', age:0};
+const handleSubmit=(event: ReactElement)=>{
+  event.preventDefault();
+  if(nameRef.current!==null && ageRef.current !==null){
+ person.name= nameRef.current.value;    
+ person.age=parseInt(ageRef.current.value);
+    console.log(person);
+  }
+}
+  
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">Name</label>
-        <input id="name" className="form-control" type="text"></input>
+        <input ref={nameRef} id="name" className="form-control" type="text"></input>
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">Age</label>
-        <input id="age" className="form-control" type="Number"></input>
+        <input ref={ageRef} id="age" className="form-control" type="Number"></input>
       </div>
+      <button className="btn btn-primary" type="submit">Submit</button>
     </form>
   );
 };
